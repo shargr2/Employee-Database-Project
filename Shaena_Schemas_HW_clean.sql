b0da90ad2c1345bfd59d905214bdc6c5d3293c87
shargr2 <shaenahargreaves@gmail.com>
2019-09-12 17:11:25 -0400

    schemas final

--- Shaena_Schemas_HW_clean.sql
+++ Shaena_Schemas_HW_clean.sql
@@ -126,4 +126,52 @@ WHERE
 	employees.first_name = 'Hercules' and
 	employees.last_name LIKE 'B%'
 ;
+
+/* 6. List all employees in the Sales department, including their employee number, 
+last name, first name, and department name. */
+SELECT     
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+	
+FROM 
+	((dept_manager
+INNER JOIN
+	departments ON dept_manager.dept_no = departments.dept_no)
+INNER JOIN
+	 employees ON employees.emp_no = dept_manager.emp_no)
+WHERE
+	departments.dept_name = 'Sales' 
+;
+
+/* 7. List all employees in the Sales and Development departments,
+ including their employee number, last name, first name, and department name. */
+ SELECT     
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+	
+FROM 
+	((dept_manager
+INNER JOIN
+	departments ON dept_manager.dept_no = departments.dept_no)
+INNER JOIN
+	 employees ON employees.emp_no = dept_manager.emp_no)
+WHERE
+	departments.dept_name = 'Sales' or
+	departments.dept_name = 'Development'
+;
+
+/* 8. In descending order, list the frequency count of employee last names, i.e., 
+how many employees share each last name. */
+SELECT     
+	employees.last_name,
+	COUNT(employees.last_name) AS number_of_employees
+FROM 
+	employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC
+;
 	 
